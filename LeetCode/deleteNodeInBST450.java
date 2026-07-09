3140033534eeb4e3accf0797babeaaa69df9a4c0
Sujal Jartarghar <sujaljartarghar03@gmail.com>
2026-07-09 23:58:54 +0530

    Ddelete Node In BST 450

---
+++ LeetCode/deleteNodeInBST450.java
@@ -0,0 +1,88 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class deleteNodeInBST450 {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+
+            if (root.left == null) {
+                return root.right;
+            }
+
+            if (root.right == null) {
+                return root.left;
+            }
+
+            TreeNode minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+        }
+
+        return root;
+    }
+
+    public TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        deleteNodeInBST450 obj = new deleteNodeInBST450();
+
+        TreeNode root = new TreeNode(5);
+
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+
+        root.right.right = new TreeNode(7);
+
+        System.out.print("Original BST: ");
+        obj.inorder(root);
+
+        root = obj.deleteNode(root, 3);
+
+        System.out.print("\nAfter deleting 3: ");
+        obj.inorder(root);
+    }
+}
