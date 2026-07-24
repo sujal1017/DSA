e44ee29071fb05cdf517e801840c7f160732462d
Sujal Jartarghar <sujaljartarghar03@gmail.com>
2026-07-25 00:27:18 +0530

    Print Name n times using Recurssion

--- TUF/Recurssion/printName.java
+++ TUF/Recurssion/printName.java
@@ -1,1 +1,13 @@
-    
+public class printName{
+    public void name(int n){
+        if(n>5){
+            return;
+        }
+        System.out.println("Sujal");
+        name(n+1);
+    }
+    public static void main(String[] args) {
+        printName obj = new printName();
+        obj.name(1);
+    }
+}
