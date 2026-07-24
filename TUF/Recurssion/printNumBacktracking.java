7b0d5e4a854b0962a16ba3f89e0b95dede2b2a57
Sujal Jartarghar <sujaljartarghar03@gmail.com>
2026-07-25 00:46:21 +0530

    Printing Number using Backtracking

--- TUF/Recurssion/printNumBacktracking.java
+++ TUF/Recurssion/printNumBacktracking.java
@@ -1,15 +1,15 @@
 public class printNumBacktracking {
-    public void print(int current,int n){
+    public void print(int current){
         if(current == 0){
             return;
         }
-        print(current-1,n);
+        print(current-1);
         System.out.println(current);
 
     }
     public static void main(String[] args) {
         printNumBacktracking obj = new printNumBacktracking();
-        obj.print(5, 5);
+        obj.print(5);
     }
     
 }
