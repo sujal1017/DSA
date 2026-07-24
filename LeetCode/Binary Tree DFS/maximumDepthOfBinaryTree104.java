59ee96bcaef719d1101e50848dd8ecaebee958b5
Sujal Jartarghar <sujaljartarghar03@gmail.com>
2026-07-24 18:28:17 +0530

    Maximum Depth Of Binary Tree 104

---
+++ LeetCode/Binary Tree DFS/maximumDepthOfBinaryTree104.java
@@ -0,0 +1,39 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class maximumDepthOfBinaryTree104 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static void main(String[] args) {
+
+        maximumDepthOfBinaryTree104 obj = new maximumDepthOfBinaryTree104();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        int depth = obj.maxDepth(root);
+        System.out.println("Maximum Depth = " + depth);
+    }
+}
