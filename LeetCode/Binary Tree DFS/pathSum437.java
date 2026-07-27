794dcdcbace597b3ece70f306eebec04370e074c
Sujal Jartarghar <sujaljartarghar03@gmail.com>
2026-07-27 23:55:47 +0530

    Path Sum III 437

---
+++ LeetCode/Binary Tree DFS/pathSum437.java
@@ -0,0 +1,56 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class pathSum437 {
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+        return pathSum(root.left, targetSum) + pathSum(root.right, targetSum) + sum(root, targetSum);
+    }
+
+    public int sum(TreeNode root, long targetSum) {
+        if (root == null) {
+            return 0;
+        }
+        int count = 0;
+        if (root.val == targetSum) {
+            count = 1;
+        }
+        count += sum(root.left, targetSum - root.val);
+        count += sum(root.right, targetSum - root.val);
+        return count;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(-3);
+
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(2);
+
+        root.right.right = new TreeNode(11);
+
+        root.left.left.left = new TreeNode(3);
+        root.left.left.right = new TreeNode(-2);
+
+        root.left.right.right = new TreeNode(1);
+
+        pathSum437 obj = new pathSum437();
+        int targetSum = 8;
+        int result = obj.pathSum(root, targetSum);
+        System.out.println("Number of paths = " + result);
+    }
+}
