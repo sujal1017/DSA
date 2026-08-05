f762e89aa4699c018e8da75598421895fde5a16d
Sujal Jartarghar <sujaljartarghar03@gmail.com>
2026-08-05 13:50:35 +0530

    Binary Tree Left Side View

--- LeetCode/Binary Tree BFS/binaryTreeLeftSideView.java
+++ LeetCode/Binary Tree BFS/binaryTreeLeftSideView.java
@@ -1,3 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class binaryTreeLeftSideView {
-    
+    public List<Integer> Solution(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        leftView(root,result,0);
+        return result;
+    }    
+    public void leftView(TreeNode current, List<Integer> result, int currDepth){
+        if(current == null){
+            return ;
+        }
+        if(currDepth == result.size()){
+            result.add(current.val);
+        }
+        leftView(current.left, result, currDepth+1);
+        leftView(current.right, result, currDepth+1);
+    }
+    public static void main(String[] args) {
+        binaryTreeLeftSideView obj = new binaryTreeLeftSideView();
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(4);
+
+
+        List<Integer> ans = obj.Solution(root);
+
+        System.out.println(ans);   
+    }
 }
